5c1a58b489bb2d179adc1d58b77384ea7cf7a446
Conner Holcomb <conner.holcomb.16@gmail.com>
2025-02-19 14:25:18 -0500

    finished

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -13,3 +13,15 @@
 #13
 #14
 #FizzBuzz
+
+
+
+for i in range(1, 26):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
--- rolltohit.py
+++ rolltohit.py
@@ -4,3 +4,34 @@
 #This is a guided practice. Either follow with the video or your instructor will
 #go over this in class.
 #Video Link: https://youtu.be/89G5DN0-O3k
+
+import random 
+
+random.seed()
+
+print("enter the number of dice and the hit target: ")
+
+dicecount = int(input("number of dice: "))
+hittarget = int(input("enter the hit target: "))
+
+hitcount = 0
+diceroll = 0
+
+for i in range(dicecount):
+    diceroll = random.randint(1,6)
+    
+
+if diceroll >= hittarget:
+        hitcount += 1
+        hitcount = hitcount + 1 
+
+print (f"you rolled {dicecount} dice with a hit target of {hittarget} and hit {hitcount} times.")
+
+
+
+
+
+
+
+
+
